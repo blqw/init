e761b2e3c01ba7f6f28f15ee9109f58d772f28b5
blqw.zhou <blqw@live.com>
2020-02-26 17:16:12 +0800

    优化脚本
    优化脚本

--- bat/nuget-pack.bat
+++ bat/nuget-pack.bat
@@ -1,17 +1,29 @@
+@echo off 
+echo "■■■■■■■■■■■■■■■■■■■■■■ 变量是: %1 ■■■■■■■■■■■■■■■■■■■■■■"
+
+
 del %CD%\unpkgs\*.nupkg
-dotnet pack -c release -o %CD%\unpkgs
-for /r . %%a in (unpkgs\*.nupkg) do (
-	if "%1"=="" (
-		md "d:\\nuget"
- 		dotnet nuget push "%%a" -s "d:\\nuget"
-	) else (
-		set "Key=%1" 
-		if "%Key:~1,1%"==":" (
-			md "%1%"
-			dotnet nuget push "%%a" -s "%1%"
-		) else (
-			dotnet nuget push "%%a" -s https://api.nuget.org/v3/index.json -k %1%
-		)
-	)
+if "%1"=="" (
+	dotnet build -c Debug
+) else (
+	dotnet build -c Release
 )
+echo "■■■■■■■■■■■■■■■■■■■■■■ 编译完成 ■■■■■■■■■■■■■■■■■■■■■■"
+
+if "%1"=="" (
+	dotnet pack -c Debug -o %CD%\unpkgs
+) else (
+	dotnet pack -c Release -o %CD%\unpkgs
+)
+echo "■■■■■■■■■■■■■■■■■■■■■■ 打包完成 ■■■■■■■■■■■■■■■■■■■■■■"
+for /r . %%a in (unpkgs\*.nupkg) do (
+    if "%1"=="" (
+        md "d:\appdata\nuget_local\"
+		echo "■■■■■■■■■■■■■■■■■■■■■■ 安装到本地 ■■■■■■■■■■■■■■■■■■■■■■"
+        dotnet nuget push "%%a" -s "d:\appdata\nuget_local"
+    ) else (
+        dotnet nuget push "%%a" -s https://api.nuget.org/v3/index.json -k %1%
+    )
+)
+echo "■■■■■■■■■■■■■■■■■■■■■■ 安装完成 ■■■■■■■■■■■■■■■■■■■■■■"
 del %CD%\unpkgs\*.nupkg
